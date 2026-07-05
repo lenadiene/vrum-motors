94a380ce1d6cd5c6812d960d6bb8ef98522627c6
lavsouza <lucascoder14@gmail.com>
2026-07-05 12:47:18 -0300

    Ajuste JPA

--- src/main/java/br/com/vrum/util/JPAUtil.java
+++ src/main/java/br/com/vrum/util/JPAUtil.java
@@ -3,6 +3,8 @@ package br.com.vrum.util;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JPAUtil {
 
@@ -11,7 +13,13 @@ public class JPAUtil {
 
     static {
         try {
-            factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+            Map<String, String> props = new HashMap<>();
+            props.put("jakarta.persistence.jdbc.driver", "com.mysql.cj.jdbc.Driver");
+            props.put("jakarta.persistence.jdbc.url",
+                    "jdbc:mysql://localhost:3306/vrum_motors?useSSL=false&serverTimezone=America/Sao_Paulo&allowPublicKeyRetrieval=true");
+            props.put("jakarta.persistence.jdbc.user", "root");
+            props.put("jakarta.persistence.jdbc.password", "root");
+            factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT, props);
         } catch (Exception e) {
             throw new ExceptionInInitializerError("Erro ao criar EntityManagerFactory: " + e.getMessage());
         }
